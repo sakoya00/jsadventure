28be46dcd08befedc1b4ba507598abb8ae48f89b
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2024-03-02 18:39:05 -0500

    Create trianglePrism.js

---
+++ trianglePrism.js
@@ -0,0 +1,22 @@
+//Created in the p5.js editor https://editor.p5js.org/
+
+let grid= 50;
+
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB,360,120,120,120);
+}
+
+function draw(){
+  //let colors= ["#80c4b7","#edcbd2","#e3856b"];
+  //let pick= Math.floor(Math.random()*3);
+  stroke(10);
+  strokeWeight(2);
+  background(0);
+  for (i=0; i< this.width+grid; i+=grid){
+    for (j=0; j<this.height+grid; j+=grid){
+      fill(i,j, 100,80);
+      triangle(i,j, i+grid,j-grid, i-grid, j-grid);
+    }
+  }
+}
