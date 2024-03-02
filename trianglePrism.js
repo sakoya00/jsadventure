96ff40d27eac96cc80f96a4028f929a0a114cbdc
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2024-03-02 18:54:03 -0500

    Update trianglePrism.js

--- trianglePrism.js
+++ trianglePrism.js
@@ -1,22 +1,30 @@
 //Created in the p5.js editor: editor.p5js.org
 
 let grid= 50;
+let n=160;
 
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB,360,120,120,120);
+  colorMode(HSB,360,100,100,255);
 }
 
 function draw(){
-  //let colors= ["#80c4b7","#edcbd2","#e3856b"];
-  //let pick= Math.floor(Math.random()*3);
   stroke(10);
   strokeWeight(2);
   background(0);
   for (i=0; i< this.width+grid; i+=grid){
     for (j=0; j<this.height+grid; j+=grid){
-      fill(i,j, 100,80);
+      fill(i,j,100,n);
       triangle(i,j, i+grid,j-grid, i-grid, j-grid);
     }
   }
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    n= n-5;
+  } else if (keyCode === DOWN_ARROW){
+    n= n+5;
+  }
+  print("transparency: "+ n);
 }
